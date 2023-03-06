57289353bdade49f581c946b306778360c21316d
Lucas Lancia <lucas.lancia.93@gmail.com>
2023-03-06 19:58:15 -0300

    otra otra

--- AppCoder/models.py
+++ AppCoder/models.py
@@ -4,19 +4,27 @@ class Curso(models.Model):
     nombre=models.CharField(max_length=40)
     camada=models.IntegerField(unique=True)
 
-def __str__(self):
-    return f"curso: {self.nombre}, Camada: {self.camada}"
+    def __str__(self):
+        return f"Curso: {self.nombre}, Camada: {self.camada}"
 
 class Estudiantes(models.Model):
     nombre=models.CharField(max_length=40)
-    apellido:models.CharField(max_length=40)
+    apellido=models.CharField(max_length=40)
     email=models.EmailField()
+
 
 class Profesor(models.Model):
     nombre=models.CharField(max_length=40)
-    apellido:models.CharField(max_length=40)
+    apellido=models.CharField(max_length=40)
     email=models.EmailField()
     profesion= models.CharField(max_length=40)
 
     def __str__(self):
-        return f"Profesor: {self.nombre} {self.apellido}"
+        return f"Profesor: {self.nombre},{self.apellido}"
+
+class Entregable(models.Model):
+    nombre=models.CharField(max_length=40)
+    fecha_de_entrega= models.DateField()
+    entregado= models.BooleanField()
+
+
